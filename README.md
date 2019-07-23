893afec342025681e1df85dbb4c5f23389346abc
chrisboogz <chriszbugaj@gmail.com>
2019-07-23 11:18:01 -0700

    Add https, cors, and a readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# Proxy Box
+
+### Getting started
+
+First:
+```
+npm install
+```
+
+Then, create a `local.json` file in the `config` directory. It should look like this:
+```json
+{
+  "httpPort": 4000, // The port to listen on for HTTP
+  "httpsPort": 4443, // The port to listen on for HTTPS
+  "target": "https://localhost:3000", // The proxy target
+  "cors": true // enable this to relieve CORS headaches
+}
+```
+Of those properties, `target` and at least one of `httpPort` or `httpsPort` are required.
+
+Finally:
+```
+npm start
+```
+
+**FOR HTTPS:** Because the HTTPS credentials used are self-signed, they'll throw errors in most browsers out of the box. In order to make it not do that for you, you'll need to explicitly trust the localhost certificate. Depending on what browser/OS you're using the steps to do that vary, so you're gonna have to Google it.
--- index.js
+++ index.js
@@ -1,18 +1,60 @@
 const express = require('express');
-const proxy = require('express-http-proxy');
-const config = require('config');
-
-const port = config.get('port');
-const target = config.get('target');
-
 const app = express();
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const http = require('http');
+const config = require('config');
+const proxy = require('http-proxy-middleware');
 
-app.options((req, res) => {
-  res.sendStatus(200);
-});
+const key = fs.readFileSync(path.join(__dirname, 'ssl', 'localhost.key'));
+const cert = fs.readFileSync(path.join(__dirname, 'ssl', 'localhost.crt'));
+const credentials = { key, cert };
 
-app.use(proxy(target));
+const target = config.get('target');
+const httpsPort = config.get('httpsPort');
+const httpPort = config.get('httpPort');
+const enableCors = config.get('cors');
 
-app.listen(port, () => {
-  console.log(`Proxy listening on port ${port}`);
-});
+const proxyOptions = {
+  target: target,
+  changeOrigin: true,
+  onProxyRes: (proxyRes, req) => {
+    if (enableCors) {
+      proxyRes.headers['Access-Control-Allow-Origin'] = req.get('Host');
+      proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
+    }
+  }
+};
+
+if (enableCors) {
+  app.options('/**', (req, res) => {
+    res.set('Access-Control-Allow-Origin', req.get('Host'));
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Allow-Credentials', 'true');
+    res.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.set('Allow', 'GET,POST,PUT,DELETE,OPTIONS');
+
+    res.status(200).json({});
+  });
+}
+
+
+app.use('/**', proxy(proxyOptions));
+
+
+
+
+if (httpsPort) {
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(httpsPort, () => {
+    console.log(`HTTPS running on port ${httpsPort}`);
+  });
+}
+
+if (httpPort) {
+  const httpServer = http.createServer(app);
+  httpServer.listen(httpPort, () => {
+    console.log(`HTTP running on ${httpPort}`);
+  });
+}
